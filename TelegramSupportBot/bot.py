9b9600190c0efd23bb17a1f8813f6e7d493cacba
Viktor Zaitsev <vazonon@yandex.ru>
2025-02-16 16:02:31 +0400

    cosmetics for reply hint

--- TelegramSupportBot/bot.py
+++ TelegramSupportBot/bot.py
@@ -196,7 +196,7 @@ def get_additional_message(message, req_id, status):
                 if additional_message == 'None':
                     additional_message = ''
 
-                bot.send_message(user_id, f'⚠️ Получен новый ответ на ваш запрос ID {req_id}!\n\n🧑‍💻 Ответ агента поддержки:\n{additional_message}\n\nЧтобы продолжить диалог по текущему запросу или завершить его - перейдите в пункт меню "Мои запросы" и выберите запрос с ID: {req_id}', reply_markup=markup.markup_main())
+                bot.send_message(user_id, f'⚠️ Получен новый ответ на ваш запрос ID {req_id}!\n\n🧑‍💻 Ответ агента поддержки:\n{additional_message}\n\nЧтобы *__продолжить диалог__* по текущему запросу или *__завершить его__*, выберите :\nМои запросы —> ❗️| ID: {req_id}', reply_markup=markup.markup_main(), parse_mode="MarkdownV2")
 
                 if type == 'photo':
                     bot.send_photo(user_id, photo=file_id, reply_markup=markup.markup_main())
